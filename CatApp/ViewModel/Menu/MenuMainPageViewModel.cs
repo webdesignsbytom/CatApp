500c859b7dbf6a3b110fd6d62f387243fb7654d5
Thomas Brockington <xtombrock1989@gmail.com>
2024-01-29 20:46:04 +0000

    save

--- CatApp/ViewModel/Menu/MenuMainPageViewModel.cs
+++ CatApp/ViewModel/Menu/MenuMainPageViewModel.cs
@@ -1,8 +1,27 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace CatApp.ViewModel.Menu
 {
     public partial class MenuMainPageViewModel : ObservableObject
     {
+        
+        [RelayCommand]
+        public async Task NavigateToAiCatsPage()
+        {
+            await Shell.Current.GoToAsync("///AiCatsPage");
+        }           
+        
+        [RelayCommand]
+        public async Task NavigateToGamePage()
+        {
+            await Shell.Current.GoToAsync("///Games Page");
+        }        
+        
+        [RelayCommand]
+        public async Task NavigateToMainPage()
+        {
+            await Shell.Current.GoToAsync("///MainPage");
+        }
     }
 }
