1c8fdd279ccafeca677547cf80e61dfc616cafe5
Thomas Brockington <xtombrock1989@gmail.com>
2024-02-06 19:21:58 +0000

    save broken share btn

--- CatApp/ViewModel/Menu/MenuMainPageViewModel.cs
+++ CatApp/ViewModel/Menu/MenuMainPageViewModel.cs
@@ -19,24 +19,28 @@ namespace CatApp.ViewModel.Menu
         }            
 
         [RelayCommand]
-        public async Task ShareCatApp()
+        public async Task ShareTheCatApp()
         {
-            // Share code
-            OpenAppStoreAsync();
+            Console.WriteLine("1111111111111111111111111111111111");
+            OpenShareStore();
         }
 
-        public async Task OpenAppStoreAsync()
+        public async Task OpenShareStore()
         {
+            Console.WriteLine("222222222222222222222222222222");
+            // Share code
             string storeUrl = string.Empty;
 
             // Perform platform check at runtime
             if (DeviceInfo.Platform == DevicePlatform.iOS)
             {
+                Console.WriteLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
                 // iOS App Store URL
                 storeUrl = "https://apps.apple.com/app/idYOUR_APP_ID";
             }
             else if (DeviceInfo.Platform == DevicePlatform.Android)
             {
+                Console.WriteLine("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
                 // Android Google Play Store URL
                 storeUrl = "https://play.google.com/store/apps/details?id=YOUR_PACKAGE_NAME";
             }
@@ -44,14 +48,16 @@ namespace CatApp.ViewModel.Menu
             // Check if the URL can be opened and then open it
             if (!string.IsNullOrEmpty(storeUrl) && await Launcher.CanOpenAsync(storeUrl))
             {
+                Console.WriteLine("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
                 await Launcher.OpenAsync(storeUrl);
             }
         }
 
+
         [RelayCommand]
         public async Task VisitSponsorWebsite()
         {
-            // Logic to open myecoapp.org page
+            // Open sponsor
             string url = "https://www.myecoapp.org/";
             await Browser.OpenAsync(url, BrowserLaunchMode.SystemPreferred);
         }        
