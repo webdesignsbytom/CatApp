46d1f048b2d455c0e92409d31aa497a49ebe2d5c
Thomas Brockington <xtombrock1989@gmail.com>
2024-02-06 23:04:05 +0000

    select a cat

--- CatApp/ViewModel/Games/GamesPageViewModel.cs
+++ CatApp/ViewModel/Games/GamesPageViewModel.cs
@@ -33,10 +33,14 @@ namespace CatApp.ViewModel.Games
         // Game Functions
 
         // Bitmap of cat
-        private SKBitmap catBitmap;
+        private SKBitmap cat1Bitmap;
+        private SKBitmap cat2Bitmap;
+        private SKBitmap cat3Bitmap;
+        private SKBitmap cat4Bitmap;
         // Canvas
         public SKCanvas? gameCanvas;
-
+        // Cat image
+        public int SelectedCatInt { get; set; } = 0;
         // Basic setup
         public void SetUpGame()
         {
@@ -60,8 +64,14 @@ namespace CatApp.ViewModel.Games
             string imageSource = "CatApp.Resources.Images.Games.";
 
             // Create varisou image sizes
-            using var catBitmapStream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{imageSource}cat1.png");
-            catBitmap = SKBitmap.Decode(catBitmapStream).Resize(new SKImageInfo(200, 300), SKFilterQuality.Low);
+            using var cat1BitmapStream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{imageSource}cat1.png");
+            cat1Bitmap = SKBitmap.Decode(cat1BitmapStream).Resize(new SKImageInfo(300, 600), SKFilterQuality.Low);
+            using var cat2BitmapStream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{imageSource}cat2.png");
+            cat2Bitmap = SKBitmap.Decode(cat2BitmapStream).Resize(new SKImageInfo(300, 600), SKFilterQuality.Low);
+            using var cat3BitmapStream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{imageSource}cat3.png");
+            cat3Bitmap = SKBitmap.Decode(cat3BitmapStream).Resize(new SKImageInfo(300, 600), SKFilterQuality.Low);
+            using var cat4BitmapStream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{imageSource}cat4.png");
+            cat4Bitmap = SKBitmap.Decode(cat4BitmapStream).Resize(new SKImageInfo(300, 600), SKFilterQuality.Low);
         }
 
         // Draw Game
@@ -75,11 +85,41 @@ namespace CatApp.ViewModel.Games
             }
 
             var mat = SKMatrix.CreateScale(1.0f, 1.0f);
+            var imageSizeSelected = cat1Bitmap;
 
-            var imageSizeSelected = catBitmap;
+            switch (SelectedCatInt)
+            {
+                case 0:
+                    imageSizeSelected = cat1Bitmap;
+                    break;
+                case 1:
+                    imageSizeSelected = cat2Bitmap;
+                    break;
+                case 2:
+                    imageSizeSelected = cat3Bitmap;
+                    break;
+                case 3:
+                    imageSizeSelected = cat4Bitmap;
+                    break;
+            }
 
-            var catPos = mat.Invert().MapPoint((float)110.0f, (float)220.0f);
+
+            var catPos = mat.Invert().MapPoint((float)65.0f, (float)120.0f);
             gameCanvas.DrawBitmap(imageSizeSelected, new SKPoint(catPos.X, catPos.Y), new SKPaint());
+        }
+
+        // Choose next cat 
+        [RelayCommand]
+        public async Task SwitchToNextCat()
+        {
+            if (SelectedCatInt == 3)
+            {
+                SelectedCatInt = 0;
+            }
+            else
+            {
+                SelectedCatInt++;
+            }
         }
 
         // Home navigation
